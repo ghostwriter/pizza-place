fedd322e30cf1c9533f87b72f72171fd8a34e671
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-15 17:09:56 -0600

    Update StoreTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Feature/Models/StoreTest.php
+++ tests/Feature/Models/StoreTest.php
@@ -1,22 +1,48 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature\Models;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StoreTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
-     *
-     * @return void
      */
-    public function testExample()
+    public function testGetStoreIndex(): void
     {
-        $response = $this->get('/api/store');
+        $user = User::factory()->withPersonalTeam()->create();
+
+        $response = $this->actingAs($user)->get(
+            route('stores.index')
+        );
 
         $response->assertStatus(200);
+        $response->assertSee('Stores List');
+        $response->assertViewIs('store.index');
+        $response->assertViewHas('stores');
+    }
+
+    /**
+     * A basic feature test example.
+     */
+    public function testGetStoreCreate(): void
+    {
+        $user = User::factory()->withPersonalTeam()->create();
+
+        $response = $this->actingAs($user)->get(
+            route('stores.create')
+        );
+
+        $response->assertStatus(200);
+        $response->assertViewIs('store.create');
+        $response->assertSee('Add a Store');
     }
 }
