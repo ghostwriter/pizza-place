3174e9f8ea1ee8b0987f7078810d9604572fcd5a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-15 17:09:43 -0600

    Update api.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::get('/user', static fn(Request $request) => $request->user());
-
-    Route::apiResource('stores', StoreController::class);
-    Route::apiResource('locations', LocationController::class);
 });
