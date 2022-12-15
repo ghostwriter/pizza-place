2d3b663e12dba4e00521fd2a317c93c76d77dd39
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-15 17:04:58 -0600

    Update migrations
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- database/migrations/2022_12_15_182825_create_stores_table.php
+++ database/migrations/2022_12_15_182825_create_stores_table.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Models\Location;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,23 +11,21 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('stores', function (Blueprint $table) {
+        Schema::create('stores', static function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            // $table->foreignId(Location::class);
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('stores');
     }
--- database/migrations/2022_12_15_182833_create_locations_table.php
+++ database/migrations/2022_12_15_182833_create_locations_table.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Models\Store;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,23 +11,27 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('locations', function (Blueprint $table) {
+        Schema::create('locations', static function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+
+            $table->string('street');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode');
+
+//            $table->foreignId(Store::class);
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('locations');
     }
